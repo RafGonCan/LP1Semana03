166d0c6012ecd83529a297d43d363307f4fc32ee
Rafael Canhoto <rafgoncan99@gmail.com>
2025-03-07 05:02:35 +0000

    All the projects folders and half of the first done

---
+++ RightSize/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace RightSize
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            foreach (string palavra in args)
+            {
+                //verify if the word is equal or bigger than 8
+                if (palavra.Length >= 8)
+                {
+                    Console.WriteLine("[EARLY STOP]");
+                    break;
+                }
+
+            }
+        }
+    }
+}
