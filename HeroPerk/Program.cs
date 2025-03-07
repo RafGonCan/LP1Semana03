ca3a0d0c0efba8bb74c331366c07075c4cb5acb4
Rafael Canhoto <rafgoncan99@gmail.com>
2025-03-07 05:51:26 +0000

    Hero Perks working

--- HeroPerk/Program.cs
+++ HeroPerk/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using HeroPerks;
 
 namespace HeroPerk
 {
@@ -6,7 +7,54 @@ namespace HeroPerk
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP1!");
+            if (args.Length != 1)
+            {
+                Console.WriteLine("!Unknown perk!");
+                return;
+            }
+
+            string cLetras = args[0];
+            Perks playerPerks = 0;
+
+            foreach(char letra in cLetras)
+            {
+                switch(letra)
+                {
+                    case 'w':
+                        playerPerks ^= Perks.WarpShift;
+                        break;
+                    case 'a':
+                        playerPerks ^= Perks.Stealth;
+                        break;
+                    case 's':
+                        playerPerks ^= Perks.AutoHeal;
+                        break;
+                    case 'd':
+                        playerPerks ^= Perks.DoubleJump;
+                        break;
+                    default:
+                        Console.WriteLine("!Unknown perk!");
+                        return;
+                }
+            }
+
+            if (playerPerks == 0)
+            {
+                Console.WriteLine("!No perks at all!");
+                return;
+            }
+
+            Console.WriteLine($"Perks do jogador: {playerPerks}");
+
+            if ((playerPerks & Perks.Stealth) != 0 && (playerPerks & Perks.DoubleJump) != 0);
+            {
+                Console.WriteLine("!Silent jumper!");
+            }
+
+            if ((playerPerks & Perks.AutoHeal) == 0)
+            {
+                Console.WriteLine("!Not gonna make it!");
+            }
         }
     }
 }
