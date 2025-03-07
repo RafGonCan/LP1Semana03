b1221813086eed3faf08d3db1f5db6afe54217b1
Rafael Canhoto <rafgoncan99@gmail.com>
2025-03-07 05:04:11 +0000

    second half of the first project

--- RightSize/Program.cs
+++ RightSize/Program.cs
@@ -8,13 +8,17 @@ namespace RightSize
         {
             foreach (string palavra in args)
             {
-                //verify if the word is equal or bigger than 8
+                //verify if the word is equal or bigger than 8 if so break the code
                 if (palavra.Length >= 8)
                 {
                     Console.WriteLine("[EARLY STOP]");
                     break;
                 }
-
+                //verify if some word is bigger than 3 and write it
+                if (palavra.Length > 3)
+                {
+                    Console.WriteLine(palavra);
+                }
             }
         }
     }
